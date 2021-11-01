0e103ade08c0d066053e40451f0574bf17325018
JuanPineda1985 <71187053+JuanPineda1985@users.noreply.github.com>
2021-11-01 13:05:45 -0500

    Initial commit

---
+++ backtienda-2-main/src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import userRoutes from './routes/UserRoutes.js';
+import productRoutes from './routes/ProductRoutes.js';
+
+const app = express()
+const port = process.env.PORT || 3003
+const mongoAtlasUrl = "mongodb+srv://seiya:juan3137749179@cluster0.zzl0r.mongodb.net/Guitar-Store?retryWrites=true&w=majority"
+
+app.use(express.json())
+app.use(userRoutes);
+app.use(productRoutes);
+app.use(cors({ origin: true }))
+
+
+app.listen(port, async () => {
+  try {
+    await mongoose.connect(mongoAtlasUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  }
+  catch (e) {
+    console.log("Error de conexión a la DB")
+  }
+  console.log(`Example app listening at http://localhost:${port}`)
+})
